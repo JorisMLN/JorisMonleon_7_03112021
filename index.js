a81cd73d5b99c3936146751f1497b9a764ffbcdf
Joris Monleon <monleon.joris@gmail.com>
2021-11-08 17:15:05 +0100

    install bootstrap + button/input switch

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-const valueSearch = document.getElementsByClassName('search_input').value;
+const button = document.getElementById('defaultDropdown');
+const input = document.getElementById('filtresInput');
+const menu = document.getElementById('menu');
+
+const switchToInput = () => {
+  button.classList.add('hidden');
+  input.classList.remove('hidden');
+  menu.classList.add('show');
+}
+
+const switchToButton = () => {
+  button.classList.remove('hidden');
+  input.classList.add('hidden');
+  menu.classList.remove('show');
+}
+
+button.addEventListener('click', switchToInput);
+input.addEventListener('focusout', switchToButton)
+
