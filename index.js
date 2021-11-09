d72f95e6f5889e84c1e4bf365dd03955f97e256b
Joris Monleon <monleon.joris@gmail.com>
2021-11-09 10:46:17 +0100

    Add 3 dropdown + ui OK

--- index.js
+++ index.js
@@ -1,19 +1,66 @@
-const button = document.getElementById('defaultDropdown');
-const input = document.getElementById('filtresInput');
-const menu = document.getElementById('menu');
 
-const switchToInput = () => {
-  button.classList.add('hidden');
-  input.classList.remove('hidden');
-  menu.classList.add('show');
+
+//  - - - - - - - - - - - - - - - - - - - - -
+// Gestion dropdown INGREDIENTS
+const btnIngredients = document.getElementById('btnIngredients');
+const inputIngredients = document.getElementById('inputIngredients');
+const menuIngredients = document.getElementById('menuIngredients');
+
+const switchToInput1 = () => {
+  btnIngredients.classList.add('hidden');
+  inputIngredients.classList.remove('hidden');
+  menuIngredients.classList.add('show');
 }
 
-const switchToButton = () => {
-  button.classList.remove('hidden');
-  input.classList.add('hidden');
-  menu.classList.remove('show');
+const switchTobtn1 = () => {
+  btnIngredients.classList.remove('hidden');
+  inputIngredients.classList.add('hidden');
+  menuIngredients.classList.remove('show');
 }
 
-button.addEventListener('click', switchToInput);
-input.addEventListener('focusout', switchToButton)
+btnIngredients.addEventListener('click', switchToInput1);
+inputIngredients.addEventListener('focusout', switchTobtn1)
 
+
+//  - - - - - - - - - - - - - - - - - - - - -
+// Gestion dropdown APPAREILS
+const btnAppareils = document.getElementById('btnAppareils');
+const inputAppareils = document.getElementById('inputAppareils');
+const menuAppareils = document.getElementById('menuAppareils');
+
+const switchToInput2 = () => {
+  btnAppareils.classList.add('hidden');
+  inputAppareils.classList.remove('hidden');
+  menuAppareils.classList.add('show');
+}
+
+const switchTobtn2 = () => {
+  btnAppareils.classList.remove('hidden');
+  inputAppareils.classList.add('hidden');
+  menuAppareils.classList.remove('show');
+}
+
+btnAppareils.addEventListener('click', switchToInput2);
+inputAppareils.addEventListener('focusout', switchTobtn2)
+
+
+//  - - - - - - - - - - - - - - - - - - - - -
+// Gestion dropdown INGREDIENTS
+const btnUstensiles = document.getElementById('btnUstensiles');
+const inputUstensiles = document.getElementById('inputUstensiles');
+const menuUstensiles = document.getElementById('menuUstensiles');
+
+const switchToInput3 = () => {
+  btnUstensiles.classList.add('hidden');
+  inputUstensiles.classList.remove('hidden');
+  menuUstensiles.classList.add('show');
+}
+
+const switchTobtn3 = () => {
+  btnUstensiles.classList.remove('hidden');
+  inputUstensiles.classList.add('hidden');
+  menuUstensiles.classList.remove('show');
+}
+
+btnUstensiles.addEventListener('click', switchToInput3);
+inputUstensiles.addEventListener('focusout', switchTobtn3)
