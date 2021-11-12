5f2c9562bed91cf221e27a97089b9718c30f3aaa
JorisMLN <monleon.joris@gmail.com>
2021-11-12 10:24:47 +0100

    Add loop display result

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
+import database from './database.js';
 
+
+//  - - - - - - - - - - - - - - - - - - - - -
+// Result display loop
+const resultDisplay = (database) => {
+  let htmlDisplayBloc = document.getElementById('result');
+  let htmlString = '';
+
+  database.forEach(element => {
+    htmlString += `
+      <div class="card" style="width: 18rem;">
+        <img src="images/test.jpg" class="card-img-top" alt="chien de traineau">
+        <div class="card-body">
+          <h5 class="card-title">${element.name} ${element.id}</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+            content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+      `
+  });
+
+  htmlDisplayBloc.innerHTML = htmlString;
+};
+
+resultDisplay(database);
 
 //  - - - - - - - - - - - - - - - - - - - - -
 // Gestion dropdown INGREDIENTS
