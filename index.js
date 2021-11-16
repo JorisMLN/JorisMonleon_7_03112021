19cf9c9ec9f2ac35f8d267c5d2fa83bd109c13f2
Joris Monleon <monleon.joris@gmail.com>
2021-11-16 14:56:33 +0100

    MàJ dropdown(dynamique data (sans les exceptions))

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ const resultDisplay = (database) => {
 
 resultDisplay(database);
 
+
 //  - - - - - - - - - - - - - - - - - - - - -
 // Gestion dropdown INGREDIENTS
 const btnIngredients = document.getElementById('btnIngredients');
@@ -37,15 +38,32 @@ const switchToInput1 = () => {
   inputIngredients.classList.remove('hidden');
   menuIngredients.classList.add('show');
 }
-
 const switchTobtn1 = () => {
   btnIngredients.classList.remove('hidden');
   inputIngredients.classList.add('hidden');
   menuIngredients.classList.remove('show');
 }
-
 btnIngredients.addEventListener('click', switchToInput1);
 inputIngredients.addEventListener('focusout', switchTobtn1)
+
+// Ingredient Dropdown display loop
+const ingredientDropdownLoop = (database) => {
+  let resultIngredients = new Set([]);
+  database.forEach(recette => {
+    recette.ingredients.forEach(elm => {
+      resultIngredients.add(elm.ingredient)
+    })
+  })
+
+  let menuIngredients = document.getElementById('menuIngredients');
+  let htmlUl = '';
+  resultIngredients.forEach(ingredient => {
+    htmlUl += `<li><a class="dropdown-item" href="#">${ingredient}</a></li>`
+  })
+  menuIngredients.innerHTML = htmlUl;
+};
+
+ingredientDropdownLoop(database);
 
 
 //  - - - - - - - - - - - - - - - - - - - - -
@@ -65,9 +83,25 @@ const switchTobtn2 = () => {
   inputAppareils.classList.add('hidden');
   menuAppareils.classList.remove('show');
 }
-
 btnAppareils.addEventListener('click', switchToInput2);
 inputAppareils.addEventListener('focusout', switchTobtn2)
+
+// Appareils Dropdown display loop
+const appareilsDropdownLoop = (database) => {
+  let resultAppareils = new Set([]);
+  database.forEach(recette => {
+    resultAppareils.add(recette.appliance)
+  })
+
+  let menuAppareils = document.getElementById('menuAppareils');
+  let htmlUl = '';
+  resultAppareils.forEach(appliance => {
+    htmlUl += `<li><a class="dropdown-item" href="#">${appliance}</a></li>`
+  })
+  menuAppareils.innerHTML = htmlUl;
+};
+
+appareilsDropdownLoop(database);
 
 
 //  - - - - - - - - - - - - - - - - - - - - -
@@ -90,3 +124,22 @@ const switchTobtn3 = () => {
 
 btnUstensiles.addEventListener('click', switchToInput3);
 inputUstensiles.addEventListener('focusout', switchTobtn3)
+
+// Appareils Dropdown display loop
+const ustensilsDropdownLoop = (database) => {
+  let resultUstensils = new Set([]);
+  database.forEach(recette => {
+    recette.ustensils.forEach(elm => {
+      resultUstensils.add(elm)
+    })
+  })
+
+  let menuUstensiles = document.getElementById('menuUstensiles');
+  let htmlUl = '';
+  resultUstensils.forEach(ustensils => {
+    htmlUl += `<li><a class="dropdown-item" href="#">${ustensils}</a></li>`
+  })
+  menuUstensiles.innerHTML = htmlUl;
+};
+
+ustensilsDropdownLoop(database);
