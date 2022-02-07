b9d8c3e81b00ea0ca609efcecd6b23d6e1c69823
Joris Monleon <monleon.joris@gmail.com>
2022-02-07 11:27:44 +0100

    Add Filtered/NewDatabase loop

--- filteredData.js
+++ filteredData.js
@@ -1,26 +1,36 @@
 import database from './database.js';
 
 const filtered = (database) => {
-  let arrayData = [];
 
-  database.forEach(recette => {
-    let payloadObjet = {};
 
-    payloadObjet.id = recette.id;
-    payloadObjet.name = recette.name.removeDiacritics();
-    payloadObjet.description = recette.description.removeDiacritics();
-    payloadObjet.appareils = recette.appliance.removeDiacritics();
-    payloadObjet.ustensils = recette.ustensils;
+  let newDatabase = [];
+  const iteratorData = database.keys();
 
-    payloadObjet.ingredients = [];
-    recette.ingredients.forEach(elm => {
-      payloadObjet.ingredients.push(elm.ingredient)
-    })
+  for (const key of iteratorData) {
+    let element = {};
 
-    arrayData.push(payloadObjet);
-  });
+    element.id = database[key].id;
+    element.name = database[key].name.removeDiacritics();
+    element.description = database[key].description.removeDiacritics();
+    element.appareil = database[key].appliance.removeDiacritics();
+    element.ustensils = [];
+    element.ingredients = [];
 
-  return arrayData;
+    const iteratorUstensils = database[key].ustensils.keys();
+    for (const keyUstensil of iteratorUstensils) {
+      element.ustensils.push(database[key].ustensils[keyUstensil].removeDiacritics());
+    }
+
+    const iteratorIngredients = database[key].ingredients.keys();
+    for (const keyIngredient of iteratorIngredients) {
+      element.ingredients.push(database[key].ingredients[keyIngredient].ingredient.removeDiacritics());
+    }
+
+    newDatabase.push(element);
+  }
+
+  console.log(newDatabase);
+  return newDatabase;
 }
 
 //  - - - - - - - - - - - - - - - - - - - - -
